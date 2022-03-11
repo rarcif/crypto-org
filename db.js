6f7eb63631948fbd7d64d5984e5a9bdfc2116897
StakePool <michelleidson@gmail.com>
2022-03-11 15:48:45 -0300

    Config db

--- db.js
+++ db.js
@@ -3,7 +3,12 @@ const Sequelize = require('sequelize');
 const sequelize = new Sequelize(process.env.URL_DATABASE,{
   dialect: 'postgres',
   logging: false,
-
+  pool: {
+    max: 20,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
 })
 
   sequelize.sync();
